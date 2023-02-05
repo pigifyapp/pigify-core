091c2eb4bd9ccf7d77c7cbddda6f8ebfddd1957b
dresnite <giantquartz@gmail.com>
2023-02-05 14:36:03 +0100

    Simplify the code architecture

---
+++ contracts/PigifyToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPLv3
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PigifyToken is ERC20 {
+
+    constructor() ERC20("Pigify", "PGY") {
+        _mint(msg.sender, 2000000000 * 10 ** decimals());
+    }
+
+}
