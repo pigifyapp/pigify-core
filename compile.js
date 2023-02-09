f7ef55c41f2bc40e69a672a38dba52c8eb9c52b8
andresbytes <giantquartz@gmail.com>
2023-02-09 02:27:26 +0100

    compile: Add remaining sources

--- compile.js
+++ compile.js
@@ -2,14 +2,64 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const contractPath = path.resolve(__dirname, "contracts", "Pigify.sol");
-const source = fs.readFileSync(contractPath, "utf8");
+const contractPath = path.resolve(__dirname, "contracts");
 
 const input = {
     language: "Solidity",
     sources: {
+        "@openzeppelin/contracts/token/ERC20/IERC20.sol": {
+            content: fs.readFileSync(
+              path.resolve(__dirname, "node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol"),
+              "utf8"
+            )
+        },
+        "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol": {
+            content: fs.readFileSync(
+              path.resolve(__dirname, "node_modules/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol"),
+              "utf8"
+            )
+        },
+        "@openzeppelin/contracts/utils/Context.sol": {
+            content: fs.readFileSync(
+              path.resolve(__dirname, "node_modules/@openzeppelin/contracts/utils/Context.sol"),
+              "utf8"
+            )
+        },
+        "@openzeppelin/contracts/token/ERC20/ERC20.sol": {
+            content: fs.readFileSync(
+              path.resolve(__dirname, "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol"),
+              "utf8"
+            )
+        },
+        "interfaces/IERC20Minimal.sol": {
+            content: fs.readFileSync(
+              path.resolve(contractPath, "interfaces", "IERC20Minimal.sol"),
+              "utf8"
+            )
+        },
+        "PigifyTokenRegistrar.sol": {
+            content: fs.readFileSync(
+              path.resolve(contractPath, "PigifyTokenRegistrar.sol"),
+              "utf8"
+            )
+        },
+        "PigifyTokenPool.sol": {
+            content: fs.readFileSync(
+              path.resolve(contractPath, "PigifyTokenPool.sol"),
+              "utf8"
+            )
+        },
+        "PigifyNativeToken.sol": {
+            content: fs.readFileSync(
+              path.resolve(contractPath, "PigifyNativeToken.sol"),
+              "utf8"
+            )
+        },
         "Pigify.sol": {
-            content: source
+            content: fs.readFileSync(
+              path.resolve(contractPath, "Pigify.sol"),
+              "utf8"
+            )
         }
     },
     settings: {
