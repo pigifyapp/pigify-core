ebbdeab5ac54fa02f5e67040d538f8d0f540ae45
andresbytes <giantquartz@gmail.com>
2023-02-20 03:00:29 +0100

    Pigify: Update USDT address

--- contracts/Pigify.sol
+++ contracts/Pigify.sol
@@ -8,7 +8,7 @@ contract Pigify is PigifyTokenPool, PigifyNativeToken {
 
     constructor() {
         _registerToken(Token.PGY, address(this));
-        _registerToken(Token.USDT, 0xeC357C27a26E94955eb415633404495044Ba7fcd);
+        _registerToken(Token.USDT, 0x5AB6F31B29Fc2021436B3Be57dE83Ead3286fdc7);
         _registerToken(Token.USDC, 0x466595626333c55fa7d7Ad6265D46bA5fDbBDd99);
     }
 
